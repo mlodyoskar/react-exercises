5ea5c079182f32d852fe652b78fba83a6db57565
Anna Kuchcinska <anna_kuchcinska@connect-tech.sncf>
2025-01-09 18:32:16 +0100

    Counter and ToDo finished

--- src/tasks/Todo.tsx
+++ src/tasks/Todo.tsx
@@ -1,4 +1,34 @@
+import {useState} from "react";
+
+type Task = {
+    id: number,
+    name: string,
+    isCompleted: boolean
+}
+
 export const Todo = () => {
+    const [task, setTask] = useState('')
+    const [tasks, setTasks] = useState<Task[]>([])
+
+    const addTask = () => {
+        if (task === '') {
+            return
+        }
+        setTasks((tasks) => [...tasks, {name: task, isCompleted: false, id: Date.now()}])
+        setTask('')
+    }
+
+    const deleteTask = (id: number) => {
+        setTasks((tasks) =>
+            tasks.filter((task) => task.id != id)
+        )
+    }
+
+    const completeTask = (id: number) => {
+        setTasks((tasks) => tasks.map((task) =>
+            task.id === id ? {...task, isCompleted: !task.isCompleted } : task))
+    }
+
  return (
   <div>
    <h1>To-Do List</h1>
@@ -7,14 +37,25 @@ export const Todo = () => {
     <li>Usuń task</li>
     <li>Zaznacz jako zakończony</li>
    </ul>
-   <input type="text" placeholder="Add a new task" />
-   <button>Add</button>
+   <input type="text" placeholder="Add a new task" onChange={e =>setTask(e.target.value)} value={task}/>
+   <button onClick={() => {
+        addTask()
+   }}>Add</button>
    <ul>
-    <li style={{ textDecoration: false ? "line-through" : "none" }}>
+       {tasks.map((task) => (
+           <li style={{ textDecoration: task.isCompleted ? "line-through" : "none" }} key={task.id}>{task.name}
+               <button onClick={() => {
+                   completeTask(task.id)
+               }}>{task.isCompleted ? "Undo" : "Complete"}</button>
+               <button onClick={() => {
+                   deleteTask(task.id)
+               }}>Delete</button>
+           </li>))}
+       {/*    <li style={{ textDecoration: false ? "line-through" : "none" }}>
      Wynieść śmieci
      <button>{false ? "Undo" : "Complete"}</button>
      <button>Delete</button>
-    </li>
+    </li>*/}
    </ul>
   </div>
  );

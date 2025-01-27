b5bab280c998ce3b76cceb256fbebc3354ebceb0
Anna Kuchcinska <anna_kuchcinska@connect-tech.sncf>
2025-01-27 09:19:42 +0100

    Import of data from API, mapping in table PostsView

--- src/tasks/PostsTable.tsx
+++ src/tasks/PostsTable.tsx
@@ -6,53 +6,101 @@
 // 1. Dodaj input gdzie bedzie mozna wpisac
 // id posta do pobrania i kliknac przycisk "Pobierz"
 
+import {useEffect, useState} from "react";
+
+type Post = {
+  userId: number,
+  id: number,
+  title: string,
+  body: string
+}
+
+const getAllPosts = async () => {
+  const results = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const data = await results.json()
+  return data as Post[]
+}
+
+const getPostById = async (id) => {
+  const foundPost = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+  const data = await foundPost.json()
+  return data as Post
+}
+
 const PostsView = () => {
+
+  const [posts, setPosts] = useState<Post[]>()
+
+  const [postID, setPostId] = useState<number>()
+
+  const handleClick = async () => {
+    const results = await getPostById(postID)
+    setPosts([results])
+    console.log('handleClick()')
+  }
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const data = await getAllPosts()
+      setPosts(data)
+    }
+    fetchPosts()
+
+  }, [])
+
+  useEffect(() => {
+    const fetch = async (id: number | undefined) => {
+      const data = await getPostById(id)
+      setPosts([data])
+    }
+    fetch(postID)
+  }, [postID]);
+
+
   return (
-    <table className="min-w-full divide-y divide-gray-200">
-      <thead className="bg-gray-50">
-        <tr>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Post id
-          </th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Title
-          </th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Body
-          </th>
-        </tr>
-      </thead>
-      <tbody className="bg-white divide-y divide-gray-200">
-        <tr className="hover:bg-gray-100">
-          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-            1
-          </td>
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-            Lorem
-          </td>
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae
-            quaerat eveniet tempora quas. Rem doloremque pariatur omnis.
-            Doloremque voluptatem cupiditate, delectus exercitationem veniam
-            molestias facere, a nostrum adipisci minus assumenda.
-          </td>
-        </tr>
-        <tr className="hover:bg-gray-100">
-          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-            2
-          </td>
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-            Lorem, ipsum.
-          </td>
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae
-            quaerat eveniet tempora quas. Rem doloremque pariatur omnis.
-            Doloremque voluptatem cupiditate, delectus exercitationem veniam
-            molestias facere, a nostrum adipisci minus assumenda.
-          </td>
-        </tr>
-      </tbody>
-    </table>
+      <>
+        <input value={postID} onChange={(e) => {
+          setPostId(parseInt(e.target.value))
+        }
+        }/>
+        <button onClick={handleClick}>Pobierz</button>
+        <table className="min-w-full divide-y divide-gray-200">
+          <thead className="bg-gray-50">
+          <tr>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Post id
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Title
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Body
+            </th>
+          </tr>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
+
+
+          {posts?.map(({id, title, body}) => { //map must be above the return because it returns JSX
+            return (
+                <tr key={id} className="hover:bg-gray-100">
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    {id}
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                    {title}
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                    {body}
+                  </td>
+                </tr>
+            )
+          })
+          }
+          </tbody>
+        </table>
+
+      </>
   );
 };
 
